be23ac19483e972968bb31d1d39a0378fcbaee44
Sergiy_Karpichenko <151627147+Karpap99@users.noreply.github.com>
2026-03-11 09:03:51 +0200

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['https://snippet-vault-9f26.vercel.app'],
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
 
   await app.listen(process.env.PORT ?? 3100);
